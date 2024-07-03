36af2f6bfb5a2e7cb1726fd569d606404f1e02ce
“hyperball123” <sufiwork2001@gmail.com>
2024-07-03 20:56:43 +0530

    two way data binding

--- src/app/components/data-binding/data-binding.component.ts
+++ src/app/components/data-binding/data-binding.component.ts
@@ -1,23 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-data-binding',
   standalone: true,
-  imports: [],
+  imports: [FormsModule ],
   templateUrl: './data-binding.component.html',
   styleUrl: './data-binding.component.css'
 })
 export class DataBindingComponent {
   courseName: string = "Angular 18";
-
+  stateName: string = "Maharashtra"
   inputType = "checkbox";
-  
+  myClassName: string = "bg-primary";
   rollNo: number = 123;
   
   isIndian: boolean = false;
   
   currentDate: Date = new Date();
+  firstName = signal("Sufiyan shaikh");
 
   constructor() {
+  }
+
+  showAlert(messsage: string){
+    alert(messsage)
+  } 
+
+  changeCourseName() {
+    this.courseName = "React Js 20";
+    this.firstName.set("Sachin tendulkar")
   }
 }
